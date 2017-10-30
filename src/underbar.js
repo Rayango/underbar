0d26744f0f4ebbda0eca881432d0d66f4f35b0e7
Ryan Ngo <rayango92@gmail.com>
2017-10-30 13:07:44 -0700

    completed indexOf to reject

--- src/underbar.js
+++ src/underbar.js
@@ -80,12 +80,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filteredCollection = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (test(collection[i])) {
+        filteredCollection.push(collection[i]);
+      }
+    }
+    return filteredCollection;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejectedCollection = []
+    for (var i = 0; i < collection.length; i++) {
+      if (!test(collection[i])) {
+        rejectedCollection.push(collection[i]);
+      }
+    }
+    return rejectedCollection;
   };
 
   // Produce a duplicate-free version of the array.
